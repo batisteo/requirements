af4fa51e3c23fcd3c2a8c5f383cae18adb614a02
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-04-10 14:10:22 +0200

    feat: Modify display of the CLI command

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,7 +3,37 @@ use requirements::{self, Requirement};
 use std::{env, fs, path::Path};
 use walkdir::DirEntry;
 
-fn find_req_files() -> Result<Vec<DirEntry>, GlobError> {
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        parse_requirements(Path::new(&args[1]))
+    } else {
+        for path in find_requirement_files().unwrap() {
+            parse_requirements(&path.path())
+        }
+    }
+}
+
+fn parse_requirements(path: &Path) -> () {
+    let content = fs::read_to_string(&path).expect("Cannot read file");
+    let requirement_file: Vec<Requirement> = requirements::parse(&content).unwrap();
+
+    for requirement in requirement_file.into_iter() {
+        if let Some(name) = requirement.name {
+            print!("{} ", name);
+        }
+        if let Some(comment) = requirement.comment {
+            print!("({}) ", comment)
+        }
+        for (comparison, version) in requirement.specs.into_iter() {
+            print!("{}{} ", comparison, version);
+        }
+        println!();
+    }
+}
+
+fn find_requirement_files() -> Result<Vec<DirEntry>, GlobError> {
     let globwalker = globwalk::GlobWalkerBuilder::from_patterns(".", &["*req*.{txt,in}"])
         .max_depth(4)
         .follow_links(true)
@@ -12,27 +42,4 @@ fn find_req_files() -> Result<Vec<DirEntry>, GlobError> {
         .filter_map(Result::ok)
         .collect();
     Ok(globwalker)
-}
-
-fn parse_requirements(path: &Path) -> () {
-    let content = fs::read_to_string(&path).expect("Cannot read file");
-    let requirement_file: Vec<Requirement> = requirements::parse(&content).unwrap();
-    for requirement in requirement_file.into_iter() {
-        println!("{:?}", requirement.line);
-    }
-}
-
-fn show_parsing(args: &Vec<String>) -> () {
-    if args.len() > 1 {
-        parse_requirements(Path::new(&args[1]))
-    } else {
-        for path in find_req_files().unwrap() {
-            parse_requirements(&path.path())
-        }
-    }
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    show_parsing(&args);
 }
